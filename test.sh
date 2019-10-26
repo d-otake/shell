f55d371830544011ebc49efafea713332080b5bd
d-otake <pierrotlefou.mas@gmail.com>
2019-10-26 13:38:09 +0900

    ファイル読み込み機能実装

--- test.sh
+++ test.sh
@@ -1,14 +1,20 @@
 
 #!/bin/bash
-read str1
-read str2
 
+# 外部ファイル読み込み
+str1=$(<input1.txt)
+str2=$(<input2.txt)
+
+# 分の10の位を取得する
 today=$(date +'%M' | cut -c 1)
 echo ${today}
-#echo ${today} | cut -c 1
+echo ${str1}
+echo ${str2}
 
+# 偶数の場合足し算、奇数の場合掛け算をおこなう
 if [ ${today}%2 = 0 ]; then
-  echo $((${str1} + ${str2}))
+  echo $(date +'%Y/%m/%d %H:%M:%S') $((${str1} + ${str2})) >> result.log
 else
-  echo $((${str1} * ${str2}))
+  echo $(date +'%Y/%m/%d %H:%M:%S') $((${str1} * ${str2})) >> result.log
 fi
+
