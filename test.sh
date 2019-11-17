a1bc746796e57a008cbd6f29990fdf95394c0a31
d-otake <pierrotlefou.mas@gmail.com>
2019-11-18 00:43:41 +0900

    inputfile名修正
    test.sh 修正

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-FileName="input1.txt"
+FileName="input.txt"
 
 # 外部ファイル読み込み
 # str1=$(<input1.txt)
@@ -16,7 +16,7 @@ today=$(date +'%M' | cut -c 1)
 # echo ${str2}
 
 # 偶数の場合足し算、奇数の場合掛け算をおこなう
-if [ ${today}%2 = 0 ]; then
+if [ $((${today} % 2)) == 0 ]; then
   echo $(date +'%Y/%m/%d %H:%M:%S') $((${str1} + ${str2})) >> result.log
 else
   echo $(date +'%Y/%m/%d %H:%M:%S') $((${str1} * ${str2})) >> result.log
